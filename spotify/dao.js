9a7dd33839475f01c1a818ed21125d7fbbd3f7c7
BillQK <billngo47@gmail.com>
2023-12-10 16:34:55 -0500

    remove refresh

--- spotify/dao.js
+++ spotify/dao.js
@@ -22,32 +22,3 @@ export const searchSpotify = async (query, accessToken) => {
   return response.data.albums.items;
 };
 
-export const refreshSpotifyToken = async () => {
-  const credentials = `${clientId}:${clientSecret}`;
-  const encodedCredentials = Buffer.from(credentials).toString("base64");
-
-  const data = qs.stringify({
-    grant_type: "refresh_token",
-    refresh_token: refreshToken,
-  });
-
-  try {
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      data,
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Authorization: `Basic ${encodedCredentials}`,
-        },
-      }
-    );
-
-    const newAccessToken = response.data.access_token;
-    // Save or process the new access token as needed
-    return newAccessToken;
-  } catch (error) {
-    console.error("Error refreshing token:", error);
-    return null;
-  }
-};
