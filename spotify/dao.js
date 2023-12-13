462787406d7e222dd67a200d329eb1cdcbd74dbb
BillQK <billngo47@gmail.com>
2023-12-13 00:59:11 -0500

    fix error api

--- spotify/dao.js
+++ spotify/dao.js
@@ -7,6 +7,11 @@ const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
 export const searchSpotify = async (query, accessToken) => {
   // Make a request to the Spotify API with the provided access token
+  // Check if the query is provided and is not empty
+  if (!query || query.trim() === "") {
+    // Return a default response or an empty array if no query is provided
+    return [];
+  }
   try {
     const response = await axios.get(process.env.SPOTIFY_BASE_API, {
       params: {
@@ -22,6 +27,6 @@ export const searchSpotify = async (query, accessToken) => {
     return response.data.albums.items;
   } catch (error) {
     console.log(error);
-    throw error
+    throw error;
   }
 };
--- spotify/routes.js
+++ spotify/routes.js
@@ -2,15 +2,22 @@ import * as dao from "./dao.js";
 
 function SpotifyRoutes(app) {
   const searchSpotifyHandler = async (req, res) => {
-    let { q, accessToken } = req.query;
+    const { q, accessToken } = req.query;
+
+    // Check if a query is provided
+    if (!q || q.trim() === "") {
+      // If the query is empty, return a default response (e.g., empty array)
+      res.json([]);
+      return;
+    }
 
     try {
       const searchResult = await dao.searchSpotify(q, accessToken);
       res.json(searchResult);
     } catch (error) {
-      if (error) {
-        res.status(error.status).json(error.message);
-      }
+      console.error(error);
+      const statusCode = error.status || 500; // Use a default error code if none is provided
+      res.status(statusCode).json({ error: error.message });
     }
   };
 
