49beb34c3915f1adbb5096ea3b752831cb155d06
BillQK <billngo47@gmail.com>
2023-12-12 23:15:30 -0500

    add try catch for search spotify

--- spotify/dao.js
+++ spotify/dao.js
@@ -7,16 +7,20 @@ const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
 export const searchSpotify = async (query, accessToken) => {
   // Make a request to the Spotify API with the provided access token
-
-  const response = await axios.get(process.env.SPOTIFY_BASE_API, {
-    params: {
-      q: query,
-      type: "album",
-    },
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  return response.data.albums.items;
+  try {
+    const response = await axios.get(process.env.SPOTIFY_BASE_API, {
+      params: {
+        q: query,
+        type: "album",
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    console.log(response);
+    return response.data.albums.items;
+  } catch (error) {
+    console.log(error);
+  }
 };
